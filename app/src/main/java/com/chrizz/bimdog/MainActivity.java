72511850afdf0377207feefb5a2aa1fb850b03f0
chrispysoft <chrispysoft@icloud.com>
2019-05-04 01:16:25 +0200

    Add home fragment

--- app/src/main/java/com/chrizz/bimdog/MainActivity.java
+++ app/src/main/java/com/chrizz/bimdog/MainActivity.java
@@ -1,6 +1,10 @@
 package com.chrizz.bimdog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
 	@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		NavHostFragment hostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+		NavController navController = hostFragment.getNavController();
 	}
+	
 }
---
+++ app/src/main/java/com/chrizz/bimdog/StopListFragment.java
@@ -0,0 +1,16 @@
+package com.chrizz.bimdog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+
+public class StopListFragment extends Fragment {
+	
+	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.stop_list_fragment, container, false);
+	}
+	
+}
