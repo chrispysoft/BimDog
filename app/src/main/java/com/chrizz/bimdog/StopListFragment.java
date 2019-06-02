a28fa565a3722c062a7536b8d9e7c1382bdb4a7d
chrispysoft <chrispysoft@icloud.com>
2019-06-02 02:20:25 +0200

    Add MapView

--- app/src/main/java/com/chrizz/bimdog/StopListFragment.java
+++ app/src/main/java/com/chrizz/bimdog/StopListFragment.java
@@ -19,14 +19,25 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import java.util.ArrayList;
 import com.chrizz.bimdog.com.chrizz.bimdog.efa.EFAClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.util.Log;
 
 
-public class StopListFragment extends Fragment implements GPSTracker.GPSTrackerListener, AdapterView.OnItemClickListener {
+public class StopListFragment extends Fragment implements GPSTracker.GPSTrackerListener, AdapterView.OnItemClickListener, OnMapReadyCallback {
 	
 	private GPSTracker gpsTracker;
 	private ArrayList stops = new ArrayList<EFAClient.Stop>();
 	private ListView listView;
+	private MapView mapView;
+	private GoogleMap googleMap;
+	private Location currentLocation;
 	private TextView logView;
 	
 	
@@ -39,6 +50,9 @@ public class StopListFragment extends Fragment implements GPSTracker.GPSTrackerL
 		listView = getView().findViewById(R.id.stopListView);
 		listView.setAdapter(new StopListAdapter(getContext(), stops));
 		listView.setOnItemClickListener(this);
+		mapView = getView().findViewById(R.id.mapView);
+		mapView.onCreate(savedInstanceState);
+		mapView.getMapAsync(this);
 		logView = getView().findViewById(R.id.logTextView);
 		logView.setText("");
 		logView.setMovementMethod(new ScrollingMovementMethod());
@@ -49,16 +63,31 @@ public class StopListFragment extends Fragment implements GPSTracker.GPSTrackerL
 		super.onResume();
 		Log.i("StopListFragment", "onResume");
 		gpsTracker.startLocationUpdates();
+		mapView.onResume();
 	}
 	
 	@Override public void onPause() {
 		super.onPause();
 		Log.i("StopListFragment", "onPause");
 		gpsTracker.stopLocationUpdates();
+		mapView.onPause();
+	}
+	
+	@Override public void onDestroy() {
+		super.onDestroy();
+		mapView.onDestroy();
+	}
+	
+	@Override public void onLowMemory() {
+		super.onLowMemory();
+		mapView.onLowMemory();
 	}
 	
 	@Override public void locationUpdated(Location location) {
 		Log.i("StopListFragment", "locationUpdated");
+		currentLocation = location;
+		updateMap();
+		
 		if (EFAClient.canAccessNetwork(getActivity())) {
 			new EFAClientStopRequest().execute(location);
 		} else {
@@ -81,6 +110,22 @@ public class StopListFragment extends Fragment implements GPSTracker.GPSTrackerL
 		Navigation.findNavController(view).navigate(R.id.departureListFragment, bundle);
 	}
 	
+	@Override public void onMapReady(GoogleMap googleMap) {
+		Log.i("StopListFragment", "onMapReady");
+		googleMap.setMinZoomPreference(17);
+		this.googleMap = googleMap;
+		updateMap();
+	}
+	
+	private void updateMap() {
+		if (googleMap != null && currentLocation != null) {
+			LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+			googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+			MarkerOptions marker = new MarkerOptions();
+			marker.position(latLng);
+			googleMap.addMarker(marker);
+		}
+	}
 	
 	private class EFAClientStopRequest extends AsyncTask<Location, Void, ArrayList<EFAClient.Stop>> {
 		@Override protected ArrayList<EFAClient.Stop> doInBackground(Location... locations) {
